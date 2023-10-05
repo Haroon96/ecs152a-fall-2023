be7f070e5c0d370463aa158ed1472f10a6406034
Muhammad Haroon <m_haroon96@hotmail.com>
2023-10-05 10:09:06 -0700

    added code file separately

---
+++ week1/dpkt-example.py
@@ -0,0 +1,57 @@
+
+import dpkt
+import sys
+
+def parse_pcap(pcap_file):
+
+    # read the pcap file
+    f = open(pcap_file, 'rb')
+    pcap = dpkt.pcap.Reader(f)
+
+    # iterate over packets
+    for timestamp, data in pcap:
+
+        # convert to link layer object
+        eth = dpkt.ethernet.Ethernet(data)
+
+        # do not proceed if there is no network layer data
+        if not isinstance(eth.data, dpkt.ip.IP):
+            continue
+        
+        # extract network layer data
+        ip = eth.data
+
+        # do not proceed if there is no transport layer data
+        if not isinstance(ip.data, dpkt.tcp.TCP):
+            continue
+
+        # extract transport layer data
+        tcp = ip.data
+
+        # do not proceed if there is no application layer data
+        # here we check length because we don't know protocol yet
+        if not len(tcp.data) > 0:
+            continue
+
+        # extract application layer data
+        ## if destination port is 80, it is a http request
+        if tcp.dport == 80:
+            try:
+                http = dpkt.http.Request(tcp.data)
+                print(http.headers)
+            except:
+                print("Malformed HTTP Request packet")
+        ## if source port is 80, it is a http response
+        elif tcp.sport == 80:
+            try:
+                http = dpkt.http.Response(tcp.data)
+                print(http.headers)
+            except:
+                print("Malformed HTTP Response packet")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("No pcap file specified!")
+    else:
+        parse_pcap(sys.argv[1])
