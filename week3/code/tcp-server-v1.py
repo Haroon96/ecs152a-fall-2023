4a27aada408bf83a2364ca425e6f7a12885d198d
Muhammad Haroon <m_haroon96@hotmail.com>
2023-10-18 14:32:08 -0700

    Update tcp-server-v1.py

--- week3/code/tcp-server-v1.py
+++ week3/code/tcp-server-v1.py
@@ -11,5 +11,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         client_socket, (client_addr, client_port) = server_socket.accept()
         data = client_socket.recv(1024)
         if data == b"ping":
-            print(f"Received {data.decode()!r} from {client_addr}:{client_port}")
+            print(f"Received {data.decode()} from {client_addr}:{client_port}")
             client_socket.sendall(b"pong")
