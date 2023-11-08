834818a0700747619cdcfe86ceb421861c8821fd
Muhammad Haroon <m_haroon96@hotmail.com>
2023-11-08 05:19:10 -0800

    added sample code

---
+++ week6/code/selenium_proxy.py
@@ -0,0 +1,26 @@
+from browsermobproxy import Server
+from selenium import webdriver
+import json
+
+# create a browsermob server instance
+server = Server("browsermob-proxy/bin/browsermob-proxy")
+server.start()
+proxy = server.create_proxy(params=dict(trustAllServers=True))
+
+# create a new chromedriver instance
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--proxy-server={}".format(proxy.proxy))
+chrome_options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(options=chrome_options)
+
+# do crawling
+proxy.new_har("myhar")
+driver.get("http://www.cnn.com")
+
+# write har file
+with open('myhar.har', 'w') as f:
+    f.write(json.dumps(proxy.har))
+
+# stop server and exit
+server.stop()
+driver.quit()
